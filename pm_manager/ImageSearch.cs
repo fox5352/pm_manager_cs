3a44f9db9b9c3a32fed431fda025ec949a246500
christopher <christophervos522@gmail.com>
2024-12-19 16:38:51 +0200

    created helper code to search directorys

---
+++ pm_manager/ImageSearch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace pm_manager
+{
+    public class ImageSearch
+    {
+        public string[] Get_Images_Paths(string path)
+        {
+            // Get all image files in the specified directory, excluding subdirectories
+            string[] imageFiles = Directory.GetFiles(path, "*.jpg"); // Adjust the pattern as needed (e.g., "*.png", "*.gif", etc.)
+
+            return imageFiles;
+        }
+    }
+}
