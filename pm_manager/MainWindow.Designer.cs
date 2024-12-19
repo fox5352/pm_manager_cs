3b73821029bc9ead78f3d7105c173b74b0500ee0
christopher <christophervos522@gmail.com>
2024-12-19 16:39:56 +0200

    added image box window and settings management

--- pm_manager/MainWindow.Designer.cs
+++ pm_manager/MainWindow.Designer.cs
@@ -35,24 +35,35 @@
             this.DataView = new System.Windows.Forms.Panel();
             this.PreView = new System.Windows.Forms.Panel();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.TabView = new System.Windows.Forms.TabControl();
+            this.BibleView = new System.Windows.Forms.TabPage();
+            this.ImageView = new System.Windows.Forms.TabPage();
+            this.ImagesViewContainer = new System.Windows.Forms.FlowLayoutPanel();
+            this.ImageViewControls = new System.Windows.Forms.Panel();
+            this.AddImageBtn = new System.Windows.Forms.Button();
             this.PreviewPanel = new pm_manager.DataView();
             this.controls.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            this.TabView.SuspendLayout();
+            this.ImageView.SuspendLayout();
+            this.ImageViewControls.SuspendLayout();
             this.SuspendLayout();
             // 
             // controls
             // 
             this.controls.AccessibleName = "Controls";
             this.controls.BackColor = System.Drawing.Color.White;
+            this.controls.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.controls.Controls.Add(this.button3);
             this.controls.Controls.Add(this.button2);
             this.controls.Controls.Add(this.viewToggleBtn);
             this.controls.Dock = System.Windows.Forms.DockStyle.Top;
             this.controls.Location = new System.Drawing.Point(0, 0);
             this.controls.Name = "controls";
-            this.controls.Size = new System.Drawing.Size(993, 46);
+            this.controls.Size = new System.Drawing.Size(1096, 46);
             this.controls.TabIndex = 0;
             this.controls.Paint += new System.Windows.Forms.PaintEventHandler(this.controls_Paint);
             // 
@@ -91,33 +102,107 @@
             // DataView
             // 
             this.DataView.AccessibleName = "playlists";
+            this.DataView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.DataView.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.DataView.Location = new System.Drawing.Point(0, 524);
+            this.DataView.Location = new System.Drawing.Point(0, 535);
             this.DataView.Name = "DataView";
-            this.DataView.Size = new System.Drawing.Size(993, 37);
+            this.DataView.Size = new System.Drawing.Size(1096, 37);
             this.DataView.TabIndex = 1;
             // 
             // PreView
             // 
             this.PreView.AccessibleName = "playlist panel";
             this.PreView.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.PreView.Location = new System.Drawing.Point(0, 297);
+            this.PreView.Location = new System.Drawing.Point(0, 308);
             this.PreView.Name = "PreView";
-            this.PreView.Size = new System.Drawing.Size(993, 227);
+            this.PreView.Size = new System.Drawing.Size(1096, 227);
             this.PreView.TabIndex = 2;
             // 
             // splitContainer1
             // 
+            this.splitContainer1.BackColor = System.Drawing.Color.White;
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 46);
             this.splitContainer1.Name = "splitContainer1";
             // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.TabView);
+            // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.PreviewPanel);
-            this.splitContainer1.Size = new System.Drawing.Size(993, 251);
-            this.splitContainer1.SplitterDistance = 492;
+            this.splitContainer1.Size = new System.Drawing.Size(1096, 262);
+            this.splitContainer1.SplitterDistance = 543;
             this.splitContainer1.TabIndex = 3;
+            // 
+            // TabView
+            // 
+            this.TabView.Controls.Add(this.BibleView);
+            this.TabView.Controls.Add(this.ImageView);
+            this.TabView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TabView.Location = new System.Drawing.Point(0, 0);
+            this.TabView.Name = "TabView";
+            this.TabView.SelectedIndex = 0;
+            this.TabView.Size = new System.Drawing.Size(541, 260);
+            this.TabView.TabIndex = 0;
+            // 
+            // BibleView
+            // 
+            this.BibleView.AutoScroll = true;
+            this.BibleView.BackColor = System.Drawing.Color.White;
+            this.BibleView.Location = new System.Drawing.Point(4, 22);
+            this.BibleView.Name = "BibleView";
+            this.BibleView.Padding = new System.Windows.Forms.Padding(3);
+            this.BibleView.Size = new System.Drawing.Size(533, 234);
+            this.BibleView.TabIndex = 0;
+            this.BibleView.Text = "BibleView";
+            // 
+            // ImageView
+            // 
+            this.ImageView.BackColor = System.Drawing.Color.White;
+            this.ImageView.Controls.Add(this.ImagesViewContainer);
+            this.ImageView.Controls.Add(this.ImageViewControls);
+            this.ImageView.Location = new System.Drawing.Point(4, 22);
+            this.ImageView.Name = "ImageView";
+            this.ImageView.Padding = new System.Windows.Forms.Padding(3);
+            this.ImageView.Size = new System.Drawing.Size(533, 234);
+            this.ImageView.TabIndex = 1;
+            this.ImageView.Text = "ImageView";
+            // 
+            // ImagesViewContainer
+            // 
+            this.ImagesViewContainer.AutoScroll = true;
+            this.ImagesViewContainer.BackColor = System.Drawing.Color.Gainsboro;
+            this.ImagesViewContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ImagesViewContainer.Location = new System.Drawing.Point(3, 31);
+            this.ImagesViewContainer.Name = "ImagesViewContainer";
+            this.ImagesViewContainer.Padding = new System.Windows.Forms.Padding(8);
+            this.ImagesViewContainer.Size = new System.Drawing.Size(527, 200);
+            this.ImagesViewContainer.TabIndex = 1;
+            // 
+            // ImageViewControls
+            // 
+            this.ImageViewControls.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ImageViewControls.Controls.Add(this.AddImageBtn);
+            this.ImageViewControls.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ImageViewControls.Location = new System.Drawing.Point(3, 3);
+            this.ImageViewControls.Margin = new System.Windows.Forms.Padding(0);
+            this.ImageViewControls.Name = "ImageViewControls";
+            this.ImageViewControls.Size = new System.Drawing.Size(527, 28);
+            this.ImageViewControls.TabIndex = 0;
+            // 
+            // AddImageBtn
+            // 
+            this.AddImageBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddImageBtn.Location = new System.Drawing.Point(5, 3);
+            this.AddImageBtn.Name = "AddImageBtn";
+            this.AddImageBtn.Size = new System.Drawing.Size(58, 21);
+            this.AddImageBtn.TabIndex = 0;
+            this.AddImageBtn.Text = "Add";
+            this.AddImageBtn.UseVisualStyleBackColor = true;
+            this.AddImageBtn.Click += new System.EventHandler(this.AddImageBtn_Click);
             // 
             // PreviewPanel
             // 
@@ -125,7 +210,7 @@
             this.PreviewPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.PreviewPanel.Location = new System.Drawing.Point(0, 0);
             this.PreviewPanel.Name = "PreviewPanel";
-            this.PreviewPanel.Size = new System.Drawing.Size(497, 251);
+            this.PreviewPanel.Size = new System.Drawing.Size(547, 260);
             this.PreviewPanel.TabIndex = 0;
             // 
             // MainWindow
@@ -133,7 +218,7 @@
             this.AccessibleName = "Window";
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(993, 561);
+            this.ClientSize = new System.Drawing.Size(1096, 572);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.PreView);
             this.Controls.Add(this.DataView);
@@ -145,9 +230,13 @@
             this.Text = "Main Window";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.controls.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            this.TabView.ResumeLayout(false);
+            this.ImageView.ResumeLayout(false);
+            this.ImageViewControls.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -162,6 +251,12 @@
         private System.Windows.Forms.Button button2;
         public System.Windows.Forms.Button viewToggleBtn;
         private DataView PreviewPanel;
+        private System.Windows.Forms.TabControl TabView;
+        private System.Windows.Forms.TabPage BibleView;
+        private System.Windows.Forms.TabPage ImageView;
+        private System.Windows.Forms.Panel ImageViewControls;
+        private System.Windows.Forms.Button AddImageBtn;
+        private System.Windows.Forms.FlowLayoutPanel ImagesViewContainer;
     }
 }
 
--- pm_manager/MainWindow.cs
+++ pm_manager/MainWindow.cs
@@ -1,27 +1,93 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace pm_manager
 {
     public partial class MainWindow : Form
     {
         private ViewWindow view;
+        private SettingsModel settings;
+        private List<string> imagesFolders;
+
+        private List<string> images = new List<string>();
+
         public MainWindow()
         {
+            // Window configuration
             InitializeComponent();
             this.Size = new Size(1366, 768);
             this.FormBorderStyle = FormBorderStyle.Sizable;
+            // settings configuration
+            this.settings = new SettingsModel();
 
+            // child Window initialization
             this.view = new ViewWindow();
+
+            // ui components initialization
+            this.Render_ImageViewContainer();
         }
 
+        // helper methods
+        protected void Get_Images_from_folders()
+        {
+            this.imagesFolders = this.settings.Get_Settings().ImageFolders;
+            ImageSearch search_helper = new ImageSearch();
+
+            foreach (string path in this.imagesFolders) {
+                string[] data = search_helper.Get_Images_Paths(path);
+
+                foreach(string item in data)
+                {
+                    this.images.Add(item);
+                }
+            }
+        }
+
+        // ui methods
+
+        // tab view components
+
+        // image view container
+        protected void Render_ImageViewContainer()
+        {
+            this.Get_Images_from_folders();
+            this.ImagesViewContainer.Controls.Clear();
+
+            foreach (var imagePath in this.images)
+            {
+                ImageBox imageBox = new ImageBox
+                {
+                    Size = new Size(180, 130),
+                    Margin = new Padding(13)
+                };
+                imageBox.LoadImage(imagePath);
+                //Panel item = new Panel
+
+                //{
+                //    Size = new Size(160, 120), // Adjust size for 3 columns
+                //    BackColor = Color.LightBlue,
+                //    BorderStyle = BorderStyle.FixedSingle,
+                //    Margin = new Padding(14) // Add spacing between items
+                //};
+
+                //// Add a label to display the item number
+                //Label label = new Label
+                //{
+                //    Text = $"Item {imagePath}",
+                //    Dock = DockStyle.Fill,
+                //    TextAlign = ContentAlignment.MiddleCenter,
+                //    Font = new Font("Arial", 12, FontStyle.Bold)
+                //};
+
+                //item.Controls.Add(label);
+
+                this.ImagesViewContainer.Controls.Add(imageBox);
+            }
+        }
+
+        // UI events
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -41,6 +107,7 @@ namespace pm_manager
         {
 
         }
+        
         private void Toggle_Window()
         {
             if (this.view.IsDisposed)
@@ -70,6 +137,24 @@ namespace pm_manager
         private void button1_Click(object sender, EventArgs e)
         {
             this.Toggle_Window();
+        }
+
+        private void AddImageBtn_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = "Select a Folder Containing Images";
+
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedFolderPath = folderBrowserDialog.SelectedPath;
+                    this.imagesFolders.Add(selectedFolderPath);
+
+                    this.settings.Update_Setting<List<string>>(SettingsFields.ImageFolders, this.imagesFolders);
+                    this.Render_ImageViewContainer();
+                }
+            }
+
         }
     }
 }
