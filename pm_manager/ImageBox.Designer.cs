b972f5f259b993b8f50e040a8cc9d1aba79798dc
christopher <christophervos522@gmail.com>
2024-12-19 16:39:29 +0200

    created ImageBox component

---
+++ pm_manager/ImageBox.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace pm_manager
+{
+    partial class ImageBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImageBoxContainer = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBoxContainer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImageBoxContainer
+            // 
+            this.ImageBoxContainer.BackColor = System.Drawing.Color.White;
+            this.ImageBoxContainer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ImageBoxContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ImageBoxContainer.Location = new System.Drawing.Point(0, 0);
+            this.ImageBoxContainer.Margin = new System.Windows.Forms.Padding(0);
+            this.ImageBoxContainer.Name = "ImageBoxContainer";
+            this.ImageBoxContainer.Size = new System.Drawing.Size(150, 120);
+            this.ImageBoxContainer.TabIndex = 0;
+            this.ImageBoxContainer.TabStop = false;
+            // 
+            // ImageBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.ImageBoxContainer);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.Margin = new System.Windows.Forms.Padding(10);
+            this.Name = "ImageBox";
+            this.Size = new System.Drawing.Size(150, 120);
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBoxContainer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ImageBoxContainer;
+    }
+}
---
+++ pm_manager/ImageBox.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pm_manager
+{
+    public partial class ImageBox : UserControl
+    {
+        public ImageBox()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadImage(string imagePath)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(imagePath))
+                {
+                    throw new ArgumentException("Image path cannot be null or empty.", nameof(imagePath));
+                }
+
+                Image image = Image.FromFile(imagePath);
+                ImageBoxContainer.Image = image;
+                ImageBoxContainer.SizeMode = PictureBoxSizeMode.Zoom; // Adjust as needed (e.g., Zoom, CenterImage, etc.)
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading image: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
